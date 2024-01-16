cd9f68aa2569d59da691cfdc2566c8e5fec057d9
loco9939 <kls9939@naver.com>
2024-01-16 23:01:30 +0900

    feat(server): dotenv, mongodb, mongoose 설치

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 
+require("dotenv").config(); // Load environment variables from .env file
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost/my-asset-portfolio", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -23,13 +26,15 @@ app.get("/", async (req, res) => {
 app.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-
   try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = new User({ email, password: hashedPassword });
     await user.save();
+
     res.status(201).send("User registered successfully");
   } catch (error) {
+    console.error("Error registering user:", error);
     res.status(500).send("Error registering user");
   }
 });
