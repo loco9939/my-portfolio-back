7229e621fa0a65656588e626c0736f1fe814b004
김이주 <150204547+WATA-Yiju@users.noreply.github.com>
2024-02-10 19:31:59 +0900

    feat(자산등록): 자산 등록 API 추가 및 로그인 성공 시 자산 데이터 전달 로직 추가

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config(); // Load environment variables from .env file
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
@@ -18,6 +18,16 @@ mongoose.connect(MONGODB_URI, {
 });
 
 const User = mongoose.model("User", { email: String, password: String });
+
+const FinancialData = mongoose.model("FinancialData", {
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  cashWon: Number,
+  saving: Number,
+  stock: Number,
+  realEstate: Number,
+  debt: Number,
+  date: { type: Date, default: Date.now },
+});
 
 app.get("/", async (req, res) => {
   return res.send("Welcome to My Portfolio Server");
@@ -55,9 +65,58 @@ app.post("/signin", async (req, res) => {
       return res.status(401).send("Invalid email or password");
     }
 
-    res.status(200).send("Sign-in Successful");
+    // Fetch financial data for the user
+    const financialData = await FinancialData.findOne({ user: user._id });
+
+    if (!financialData) {
+      return res.status(200).json({});
+    }
+
+    // Include financial data in the response
+    const responseData = {
+      financialData: {
+        cashWon: financialData.cashWon,
+        saving: financialData.saving,
+        stock: financialData.stock,
+        realEstate: financialData.realEstate,
+        debt: financialData.debt,
+        date: financialData.date,
+      },
+    };
+
+    res.status(200).json(responseData);
   } catch (error) {
     res.status(500).send("Error signing in");
+  }
+});
+
+app.post("/save-financial-data", async (req, res) => {
+  const { userId, cashWon, saving, stock, realEstate, debt } = req.body;
+
+  try {
+    const user = await User.findOne({
+      email: { $regex: new RegExp(`^${userId}$`, "i") },
+    });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const financialData = new FinancialData({
+      user: user._id,
+      cashWon,
+      saving,
+      stock,
+      realEstate,
+      debt,
+    });
+
+    await financialData.save();
+
+    res.status(201).send("Finacial data saved successfully");
+  } catch (error) {
+    console.error("Error saving financial data: ", error);
+    res.status(500).send("Error saving finacial data");
   }
 });
 
